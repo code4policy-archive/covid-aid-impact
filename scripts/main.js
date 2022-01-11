ddcb4e0dd8f37d49bbc3b808fcb8f136c32c6edd
Nomko Baatar <nomko_b@yahoo.com>
2022-01-10 20:32:30 -0500

    Cleaning up the website

--- scripts/main.js
+++ scripts/main.js
@@ -44,3 +44,11 @@ $(document).ready(function(){
     $("#myCarousel").carousel("next");
   });
 });
+
+// Tableau functionality
+        var divElement = document.getElementById('viz1641859286999');
+        var vizElement = divElement.getElementsByTagName('object')[0];
+        vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px'; 
+        var scriptElement = document.createElement('script');
+        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+        vizElement.parentNode.insertBefore(scriptElement, vizElement);   
